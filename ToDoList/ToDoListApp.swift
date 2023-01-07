b296a0ced7dcb7ad9c0fa353cfb3daae0f70b9e3
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-01-07 08:03:30 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by youlserf on 7/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
